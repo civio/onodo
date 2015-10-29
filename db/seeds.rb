76f9e92d8ca686ad51b2d7b43b47cf2f7b519145
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-10-29 19:19:16 +0100

    Add DB seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,104 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+miles = Node.create!( name: 'Miles Davis',
+                          description: '', 
+                          node_type: 'trumpet')
+
+bill = Node.create!( name: 'Bill Evans', 
+                          description: '', 
+                          node_type: 'piano')
+
+herbie = Node.create!( name: 'Herbie Hancock', 
+                          description: '', 
+                          node_type: 'piano')
+
+red = Node.create!( name: 'Red Garland', 
+                          description: '', 
+                          node_type: 'piano')
+
+trane = Node.create!( name: 'John Coltrane', 
+                          description: '', 
+                          node_type: 'tenor sax')
+
+wayne = Node.create!( name: 'Wayne Shorter', 
+                          description: '', 
+                          node_type: 'tenor sax')
+
+kenny = Node.create!( name: 'Kenny Garret', 
+                          description: '', 
+                          node_type: 'alto sax')
+
+ron = Node.create!( name: 'Ron Carter', 
+                          description: '', 
+                          node_type: 'bass')
+
+paul = Node.create!( name: 'Paul Chambers', 
+                          description: '', 
+                          node_type: 'bass')
+
+dave = Node.create!( name: 'Dave Holland', 
+                          description: '', 
+                          node_type: 'bass')
+
+jimmy = Node.create!( name: 'Jimmy Cobb', 
+                          description: '', 
+                          node_type: 'drums')
+
+tony = Node.create!( name: 'Tony Williams', 
+                          description: '', 
+                          node_type: 'drums')
+
+jack = Node.create!( name: 'Jack de Johnette', 
+                          description: '', 
+                          node_type: 'drums')
+
+Relation.create!(source: miles,
+                target: bill,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: herbie,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: red,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: trane,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: wayne,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: kenny,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: ron,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: paul,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: dave,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: jimmy,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: tony,
+                relation_type: 'music')
+
+Relation.create!(source: miles,
+                target: jack,
+                relation_type: 'music')
+
