776b7c5cd2cf7a87b4fb33e35d98c7c6f9d51c42
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-10-29 19:17:50 +0100

    Setup Node & Relation Models

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,2 +1,7 @@
 class Node < ActiveRecord::Base
+  has_many :relations_as_source, foreign_key: :source_id, class_name: Relation, inverse_of: :source
+  has_many :relations_as_target, foreign_key: :target_id, class_name: Relation, inverse_of: :target
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, length: { maximum: 90 }
 end
--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,2 +1,14 @@
 class Relation < ActiveRecord::Base
+  belongs_to :source, 
+              foreign_key: :source_id, 
+              class_name: Node, 
+              inverse_of: :relations_as_source, 
+              touch: true
+  belongs_to :target, 
+              foreign_key: :target_id, 
+              class_name: Node, 
+              inverse_of: :relations_as_target, 
+              touch: true
+
+  validates :source, :target, :relation_type, presence: true
 end
