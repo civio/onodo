0671610a8e7672d9772c8834d63699fadf616a27
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-11-04 19:26:12 +0100

    Add Relations model to API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,5 @@
 class ApiController < ApplicationController
 
-  #respond_to :json
-
   # Get all Nodes
   def nodes
     render json: Node.all
@@ -32,10 +30,41 @@ class ApiController < ApplicationController
     render json: Node.select(:node_type).map(&:node_type).reject(&:blank?).uniq
   end
 
+  ###
+
+  # Get all Relations
+  def relations
+    render json: Relation.all
+  end
+
+  # Get a Relation
+  def relation
+    render json: Relation.find(params[:id])
+  end
+
+  # Create a new Relation
+  def relation_create
+    render json: Relation.create(relation_params)
+  end
+
+  # Update a Relation attribute
+  def relation_update
+    render json: Relation.update(params[:id], relation_params)
+  end
+
+  # Delete a Relation
+  def relation_destroy
+    render json: Relation.destroy(params[:id])
+  end
+
   private
 
     def node_params
-      params.require(:node).permit(:id, :name, :description, :created_at, :updated_at, :visible, :node_type) if params[:node]
+      params.require(:id, :node).permit(:name, :description, :created_at, :updated_at, :visible, :node_type) if params[:node]
+    end
+
+    def relation_params
+      params.require(:id, :source_id, :source_id).permit(:created_at, :updated_at, :relation_type) if params[:relation]
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 Rails.application.routes.draw do
 
-  resources :relations
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   root 'home#index'
 
   resources :nodes
+  resources :relations
 
   # API routes
   scope 'api' do
+    # Node routes
     get     'nodes'       => 'api#nodes'
     post    'nodes'       => 'api#node_create'
     get     'nodes/:id'   => 'api#node'
     put     'nodes/:id'   => 'api#node_update'
     delete  'nodes/:id'   => 'api#node_destroy'
     get     'nodes-types' => 'api#node_types'
+    # Relation routes
+    get     'relations'       => 'api#relations'
+    post    'relations'       => 'api#relation_create'
+    get     'relations/:id'   => 'api#relation'
+    put     'relations/:id'   => 'api#relation_update'
+    delete  'relations/:id'   => 'api#relation_destroy'
   end
 
   # Example of regular route:
