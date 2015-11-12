4d7195eeafbbe6f46a4b4533ab52b47f082bf300
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-11-12 17:51:31 +0100

    Setup login & signup views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Add agreement checkbox to sign_up form
+  before_filter :update_sanitized_params, if: :devise_controller?
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:agreement, :email, :password, :password_confirmation) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :skip => [:sessions]
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new', :as => :new_user_session
+    post 'login' => 'devise/sessions#create', :as => :user_session
+    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   # You can have the root of your site routed with "root"
   root 'home#index'
 
