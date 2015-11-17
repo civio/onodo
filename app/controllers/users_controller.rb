20afafeb8795dfe7e41d2993a33e5b0c833706a2
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-11-17 14:18:57 +0100

    Update user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,13 @@ class UsersController < ApplicationController
 
   # GET /user/:id/dashboard
   def dashboard
-
+    @user = User.find(params[:id])
+    if current_user.nil? 
+      redirect_to '/login'
+    elsif current_user != @user 
+      redirect_to '/'
+    end
+    @visualizations = Visualization.where(author_id: params[:id])
   end
 
   # GET /user/:id/settings
