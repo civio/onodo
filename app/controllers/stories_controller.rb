67b26446e23b88d3161ff5b25837713901a2396b
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-11-26 11:38:22 +0100

    Add stories actions

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,4 +4,24 @@ class StoriesController < ApplicationController
   def show
     @story = Story.find(params[:id])
   end
+
+  # GET /stories/new
+  def new
+    @story = Story.new
+  end
+
+  # GET /stories/:id/edit
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  # PATCH /stories/:id/
+  def update
+    @story = Story.find(params[:id])
+  end
+
+  # DELETE /stories/:id/
+  def destroy
+    @story = Story.find(params[:id])
+  end
 end
