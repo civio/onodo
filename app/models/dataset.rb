1c69f017e76eedd12b3d37f495f598b749fe5614
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-11-26 19:09:06 +0100

    Make Dataset, Nodes & Relations dependents of Visualization when destroy
    So when a Visualization is deleted, its Dataset and its Nodes &
    Relations are also deleted

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,8 +1,8 @@
 class Dataset < ActiveRecord::Base
   #belongs_to :author, foreign_key: :author_id, class_name: User
   belongs_to :visualization
-  has_many :nodes
-  has_many :relations
+  has_many :nodes, dependent: :destroy
+  has_many :relations, dependent: :destroy
 
   validates :visualization, presence: true
 end
--- app/models/visualization.rb
+++ app/models/visualization.rb
@@ -2,7 +2,7 @@ class Visualization < ActiveRecord::Base
   belongs_to :author, foreign_key: :author_id, class_name: User
   belongs_to :visualization
 
-  has_one :dataset
+  has_one :dataset, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
