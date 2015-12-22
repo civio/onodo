c62e5b4535c072a064c22776359a0cb49a99116c
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-12-22 17:54:03 +0100

    Update registration controller parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  # Add terms_of_service checkbox to sign_up form
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :terms_of_service) }
-  end
+  
 end
