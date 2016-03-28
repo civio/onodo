57316722686356a5d9fe9a71157b4af1b363dd52
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-03-28 13:29:17 +0200

    Add home sprites support for Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,27 @@
+// Run task as `grunt sprite`
 module.exports = function (grunt) {
   // Configure grunt
   grunt.initConfig({
     sprite:{
+      
+      sprite: {
+        src: './app/assets/images/sprites/main/*.png',
+        dest: './public/images/sprites.png',
+        destCss: './app/assets/stylesheets/scss/_sprites.scss',
+        imgPath: '/images/sprites.png',
+        'engineOpts': {
+          'imagemagick': true
+        },
+      },
+      sprite_home: {
+        src: './app/assets/images/sprites/home/*.png',
+        dest: './public/images/sprites-home.png',
+        destCss: './app/assets/stylesheets/scss/_sprites-home.scss',
+        imgPath: '/images/sprites-home.png',
+        'engineOpts': {
+          'imagemagick': true
+        },
+      },
       sprite_large: {
         src: './app/assets/images/sprites/main-2x/*.png',
         dest: './public/images/sprites-2x.png',
@@ -11,15 +31,15 @@ module.exports = function (grunt) {
           'imagemagick': true
         },
       },
-      sprite: {
-        src: './app/assets/images/sprites/main/*.png',
-        dest: './public/images/sprites.png',
-        destCss: './app/assets/stylesheets/scss/_sprites.scss',
-        imgPath: '/images/sprites.png',
+      sprite_home_large: {
+        src: './app/assets/images/sprites/home-2x/*.png',
+        dest: './public/images/sprites-home-2x.png',
+        destCss: './app/assets/stylesheets/scss/_sprites-home-2x.scss',
+        imgPath: '/images/sprites-home-2x.png',
         'engineOpts': {
           'imagemagick': true
         },
-      }
+      },
     }
   });
 
