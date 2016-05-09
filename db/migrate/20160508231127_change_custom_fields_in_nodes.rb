4fe38da7eda2570adc24c91fac0d13e3b53e6074
Eduardo Sebastián <eduardo@civio.es>
2016-05-09 13:34:38 +0200

    Fix unexpected end of string error in migration

--- db/migrate/20160508231127_change_custom_fields_in_nodes.rb
+++ db/migrate/20160508231127_change_custom_fields_in_nodes.rb
@@ -2,7 +2,7 @@ class ChangeCustomFieldsInNodes < ActiveRecord::Migration
   def change
     enable_extension 'hstore'
     rename_column :nodes, :custom_field, :custom_fields
-    change_column :nodes, :custom_fields, 'hstore USING CAST(custom_fields AS hstore)'
+    change_column :nodes, :custom_fields, 'hstore USING CAST(custom_fields AS hstore);'
 
     add_index :nodes, :custom_fields, using: :gist
   end
