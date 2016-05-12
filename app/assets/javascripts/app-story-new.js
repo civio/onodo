f347523793d351e757d7199e68c57b633ec8f9dc
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-05-12 17:22:03 +0200

    Require story name when creating a Story

--- app/assets/javascripts/app-story-new.js
+++ app/assets/javascripts/app-story-new.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
   // Listen click on Visualization Select Item to store visualization id & submit form
   $('.story-select-visualization-list .visualization-item').click(function(e){
     e.preventDefault();
+    // Validate nstory name presence
+    if ($('#story_name').val().trim() === '') {
+      $('#story_name').focus();
+      return;
+    }
     // Store visualization id in hidden input
     $('#story_visualization').val( $(this).attr('href') );
     // Submit form
