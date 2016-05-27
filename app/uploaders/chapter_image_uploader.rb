29cc8d1a9c49f43547f8eda8ae9935f135ac5a57
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-05-27 10:53:52 +0200

    Add medium size for chapter image uploader
    We need medium size for chapter edit form

--- app/uploaders/chapter_image_uploader.rb
+++ app/uploaders/chapter_image_uploader.rb
@@ -8,4 +8,8 @@ class ChapterImageUploader < BasePictureUploader
     process :resize_to_fit => [160, nil]
   end
   
+  version :medium, :from_version => :huge do
+    process :resize_to_fill => [128, 128]
+  end
+
 end
