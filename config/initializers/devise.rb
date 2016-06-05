e848b29a783fd5727a0f34c4260b93be12776ffe
Eduardo Sebastián <eduardo@civio.es>
2016-06-05 16:26:14 +0200

    Use environment variable for devise secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = '831e89c0835a4287ae8fca48c73ef8549d3de00e5456b6663d828f1d0edcde5857ebcbbab5c94e2afc44dbe73c1e62b316f6b1f4d8e68c900290868121bf60c1'
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] || ''
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
