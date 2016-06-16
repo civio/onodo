fa9b77a63ffaccbea22726e7fc2026b2b6073799
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-06-16 15:52:23 +0200

    Create format_date_for helper to get formatted date in relations

--- app/helpers/relations_helper.rb
+++ app/helpers/relations_helper.rb
@@ -1,2 +1,10 @@
 module RelationsHelper
+
+  def format_date_for(relation)
+    if relation.to.nil?
+      return "#{relation.from}"
+    else
+      return "#{relation.from} - #{relation.to}"
+    end
+  end
 end
