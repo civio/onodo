f4ad9f533b285d73358fd8c55e9e9df8ce1796a8
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-06-20 19:13:42 +0200

    Add ellipsis in relations range date when from or to values are null

--- app/helpers/relations_helper.rb
+++ app/helpers/relations_helper.rb
@@ -3,6 +3,10 @@ module RelationsHelper
   def format_date_for(relation)
     if relation.from == relation.to
       return "#{relation.from}"
+    elsif relation.from.nil?
+      return "… - #{relation.to}"
+    elsif relation.to.nil?
+      return "#{relation.from} - …"
     else
       return "#{relation.from} - #{relation.to}"
     end
