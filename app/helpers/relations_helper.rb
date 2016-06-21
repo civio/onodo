6c50f8234a52d3c6ef2c16cbd345d990be5922a7
Eduardo Sebastián <eduardo@civio.es>
2016-06-21 05:47:10 +0200

    Simplify formatter helper for relation dates

--- app/helpers/relations_helper.rb
+++ app/helpers/relations_helper.rb
@@ -1,14 +1,7 @@
 module RelationsHelper
 
   def format_date_for(relation)
-    if relation.from == relation.to
-      return "#{relation.from}"
-    elsif relation.from.nil?
-      return "… - #{relation.to}"
-    elsif relation.to.nil?
-      return "#{relation.from} - …"
-    else
-      return "#{relation.from} - #{relation.to}"
-    end
+    return relation.at if relation.transient?
+    "#{relation.from || '…'} - #{relation.to || '…'}"
   end
 end
