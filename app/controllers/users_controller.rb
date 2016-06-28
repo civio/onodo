bc29d8ceda4f97dbe1262788063d153be9eb74da
Eduardo Sebastián <eduardo@civio.es>
2016-06-28 13:34:34 +0200

    Fix authorization check for dashboard access
    Closes #225

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
 
   PAGE_SIZE = 4
 
+  before_action :authenticate_user!, except: [:show, :show_visualizations, :show_stories]
+  before_action :set_user, only: [:show, :show_visualizations, :show_stories]
+
   # GET /users/:id
   def show
-    @user = user_for params[:id]
-    # Get published visualizations from user
-    @items = visualizations_for @user, true, params[:page]
+    @items = visualizations_for @user, published: true, page: params[:page]
     @visualizations = true
     render :show
   end
@@ -16,17 +17,14 @@ class UsersController < ApplicationController
 
   # GET /users/:id/stories
   def show_stories
-    @user = user_for params[:id]
-    @items = stories_for @user, true, params[:page]
+    @items = stories_for @user, published: true, page: params[:page]
     render :show
   end
 
   # GET /dashboard
   def show_dashboard
-    return unless current_user
     @user = current_user
-    # Get all visualizations from user
-    @items = visualizations_for @user, false, params[:page]
+    @items = visualizations_for @user, published: false, page: params[:page]
     @visualizations = true
     render :show
   end
@@ -36,32 +34,29 @@ class UsersController < ApplicationController
 
   # GET /dashboard/stories
   def show_dashboard_stories
-    return unless current_user
     @user = current_user
-    @items = stories_for @user, false, params[:page]
+    @items = stories_for @user, published: false, page: params[:page]
     render :show
   end
 
   private
 
-  def user_for user_id
-    User.find(user_id)
+  def set_user
+    @user = User.find(params[:id])
   end
 
-  def visualizations_for user, published, page=nil
-    if published
-      user.visualizations.published.page(page).per(PAGE_SIZE)
-    else
-      user.visualizations.page(page).per(PAGE_SIZE)
-    end  
+  def visualizations_for user, published:, page: nil
+    visualizations = user.visualizations
+    visualizations = visualizations.published if published
+
+    visualizations.page(page).per(PAGE_SIZE)
   end
 
-  def stories_for user, published, page=nil
-    if published
-      user.stories.published.page(page).per(PAGE_SIZE)
-    else
-      user.stories.page(page).per(PAGE_SIZE)
-    end
+  def stories_for user, published:, page: nil
+    stories = user.stories
+    stories = stories.published if published
+
+    stories.page(page).per(PAGE_SIZE)
   end
 
 end
