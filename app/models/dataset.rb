0c01c5524d6d320ea6b02eefc12c3d5d4d824b8d
Eduardo Sebastián <eduardo@civio.es>
2016-07-14 19:05:57 +0200

    Ensure saving of nodes and relations when their parent dataset is saved

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,8 +1,8 @@
 class Dataset < ActiveRecord::Base
   #belongs_to :author, foreign_key: :author_id, class_name: User
   belongs_to :visualization
-  has_many :nodes, dependent: :destroy
-  has_many :relations, dependent: :destroy
+  has_many :nodes, dependent: :destroy, autosave: true
+  has_many :relations, dependent: :destroy, autosave: true
 
   validates :visualization, presence: true, uniqueness: true
 end
